18f8d0ec2c63d87c11e8231f8cf3933237497426
Sergio Menezes <sergio.filho.444859@sga.pucminas.br>
2021-11-11 23:03:17 -0300

    Produtos_update
    Update na funcionalidade de "Produtos".

--- src/mini-website-produtos.js
+++ src/mini-website-produtos.js
@@ -17,60 +17,87 @@ if (findIndex == -1) {
 
 document.querySelector("#nome_loja").innerText = listaVendedores[findIndex].nome;
 
+updateListaProdutos();
+
 function detalhesProduto(detalhes) {
-    window.alert(detalhes);
+    localStorage.setItem('maisDetalhesAtual', JSON.stringify(detalhes));
+    let findIndexProduto = listaVendedores[findIndex].catalogo.findIndex(x => x.nome === detalhes)
+    document.querySelector("#informacoes_imagem_produto").src = listaVendedores[findIndex].catalogo[findIndexProduto].imagem;
+    document.querySelector("#informacoes_nome_produto").innerText = listaVendedores[findIndex].catalogo[findIndexProduto].nome;
+    document.querySelector("#informacoes_preco_produto").innerText = "Preço: R$ " + listaVendedores[findIndex].catalogo[findIndexProduto].preco;
+    document.querySelector("#informacoes_descricao_produto").innerHTML = "<b>Descrição:<br></b>" + listaVendedores[findIndex].catalogo[findIndexProduto].descricao;
+    let organizarTextoSabores = listarSabores();
+    document.querySelector("#informacoes_sabores_produto").innerHTML = "<b>Sabores:<br></b>" + organizarTextoSabores;
+    document.querySelector("#informacoes_outras_produto").innerHTML = "<b>Outras informações:<br></b>" + listaVendedores[findIndex].catalogo[findIndexProduto].outrasInformacoes;
+    
+    function listarSabores() {
+        let sabores = listaVendedores[findIndex].catalogo[findIndexProduto].sabores;
+        let textoSabores = "";
+        if (sabores != null && sabores != "") {
+            for (let i = 0; i < (sabores.length); i++) {
+                if (i == (sabores.length - 1)) {
+                    textoSabores += sabores[i]+".";
+                }
+                else {
+                    textoSabores += sabores[i]+", "
+                }
+            }
+        }
+        return textoSabores;
+    }
 }
 
 /* Exibe os produtos disponiveis no catalogo */
-
-let mostrarProdutos = document.querySelector("#mini_website_produtos");
-let catalogoProdutos = listaVendedores[findIndex].catalogo;
-let produtosMostrados = [{"empty" : true},];
-let listaProdutos;
-if (catalogoProdutos) {
-    for (let i = 0; i < (catalogoProdutos.length); i++) {
-        produtosMostrados[i] = catalogoProdutos[i];
+function updateListaProdutos() { 
+    let mostrarProdutos = document.querySelector("#mini_website_produtos");
+    let catalogoProdutos = listaVendedores[findIndex].catalogo;
+    let produtosMostrados = [{"empty" : true},];
+    let listaProdutos;
+    if (catalogoProdutos) {
+        for (let i = 0; i < (catalogoProdutos.length); i++) {
+            produtosMostrados[i] = catalogoProdutos[i];
+            produtosMostrados.push({});
+        }
+    }
+    else {
         produtosMostrados.push({});
     }
-}
-else {
-    produtosMostrados.push({});
-}
 
 
-for (let i = 0; i < (produtosMostrados.length - 1); i++) {
-    let imagemProduto = produtosMostrados[i].imagem;
-    let nomeProduto = produtosMostrados[i].nome;
-    let descricaoProduto = produtosMostrados[i].descricao;
-    let precoProduto = produtosMostrados[i].preco;
-    let resultado;
+    for (let i = 0; i < (produtosMostrados.length - 1); i++) {
+        let imagemProduto = produtosMostrados[i].imagem;
+        let nomeProduto = produtosMostrados[i].nome;
+        let descricaoProduto = produtosMostrados[i].descricaoResumo;
+        let precoProduto = produtosMostrados[i].preco;
+        let resultado;
 
-    if (produtosMostrados[0].empty) {
-        resultado = `<div class="alert-danger" style="padding: 8px; margin-top: 32px; margin-bottom: 320px;"><h4 class="text-center"> Nenhum produto cadastrado </h4></div>`
+        if (produtosMostrados[0].empty) {
+            resultado = `<div class="alert-danger" style="padding: 8px; margin-top: 32px; margin-bottom: 320px;"><h4 class="text-center"> Nenhum produto cadastrado </h4></div>`
+        }
+        else {
+            resultado = `
+                <div class="card_produto">
+                <img src="${imagemProduto}">
+                <div class="card_produto_descricao">
+                <h4 class="text-center">${nomeProduto}</h4>
+                <p>${descricaoProduto}</p>
+                </div>
+                <div class="card_produto_preco">
+                <b>R$ ${precoProduto}</b>
+                </div>
+                <div class="card_produto_detalhes">
+                <button class="btn btn-primary" type="button" onclick="detalhesProduto('${nomeProduto}')" data-toggle="modal" data-target="#mais_detalhes"> Mais detalhes</button>
+                </div>
+                </div>
+            `;
+        }
+        if (i == 0) { 
+            listaProdutos = resultado;
+        } 
+        else {
+            listaProdutos += resultado;
+        }
     }
-    else {
-        resultado = `
-            <div class="card_produto">
-            <img src="${imagemProduto}">
-            <div class="card_produto_descricao">
-            <h4 class="text-center">${nomeProduto}</h4>
-            <p>${descricaoProduto}</p>
-            </div>
-            <div class="card_produto_preco">
-            <b>R$ ${precoProduto}</b>
-            </div>
-            <div class="card_produto_detalhes">
-            <button class="btn btn-primary" type="button" onclick="detalhesProduto('${nomeProduto}')"> Mais detalhes</button>
-            </div>
-            </div>
-        `;
-    }
-    if (i == 0) { 
-        listaProdutos = resultado;
-    } 
-    else {
-        listaProdutos += resultado;
-    }
-}
 
-mostrarProdutos.innerHTML = listaProdutos;
+    mostrarProdutos.innerHTML = listaProdutos;
+}
