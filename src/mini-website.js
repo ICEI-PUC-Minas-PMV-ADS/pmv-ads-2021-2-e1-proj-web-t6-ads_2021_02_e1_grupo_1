b877d37a1540b0c301b073011b9f9e12904b6b73
Sergio Menezes <sergio.filho.444859@sga.pucminas.br>
2021-11-23 18:25:58 -0300

    update videoapresentação
    Video de apresentação do vendedor adicionado ao mini website

--- src/mini-website.js
+++ src/mini-website.js
@@ -1,3 +1,13 @@
 document.querySelector("#nome_loja").innerText = listaVendedores[findIndex].nome;
 document.querySelector("#texto_mini_website").innerText = listaVendedores[findIndex].descricao;
 document.querySelector("#imagem_mini_website").src = listaVendedores[findIndex].imagem;
+
+let videoApresentacao = src = listaVendedores[findIndex].videoApresentacao;
+
+console.log(videoApresentacao);
+if (videoApresentacao != null && videoApresentacao != "") {
+    document.querySelector("#video_apresentacao").src = videoApresentacao;
+}
+else {
+    document.querySelector("#video_apresentacao").style = "display: none";
+}
